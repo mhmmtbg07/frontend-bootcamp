2069c40a2a10a12777067f0362212dd99ce3e671
Muhammet BAĞ <muhammetbag07@gmail.com>
2025-05-19 09:30:10 +0300

    56

---
+++ 56/app.js
@@ -0,0 +1,30 @@
+// Document output
+document.writeln("Hello World! <br>");
+document.writeln("Muhammet BAĞ <br>");
+console.clear();
+
+// Console Log
+// console.clear();
+console.log("Hello World!");
+console.log("Muhammet BAĞ");
+console.clear();
+
+let a = 4;
+let b = 2;
+$total = a*b;
+console.log(
+    "A & B numbers total:"  ,
+    a+b
+);
+console.clear();
+
+// Window
+console.log(window);
+console.clear();
+
+// Alert "Popup"
+alert("Hello World!");
+alert(
+    "A * B total: " + ($total)
+);
+
