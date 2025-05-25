253543528bcd5e21f8c94a728c8abb76e37e83bd
Muhammet BAĞ <muhammetbag07@gmail.com>
2025-05-25 09:37:29 +0300

    Course 60

---
+++ 60/app.js
@@ -0,0 +1,60 @@
+//    Arithmetic Operators
+/*
+    = 
+    +
+    - 
+    *
+    /
+    %
+    ++
+    --
+    **
+*/
+
+// "="
+let a = 15;
+console.log(a);
+console.clear();
+
+// "+"
+let num1 = 5;
+let num2 = 2;
+console.log(num1+num2);
+// "string" + "number" cant collected
+let not1 = "2";
+let not = 50;
+console.log(not1+not);
+
+// "-"
+console.log(num1-num2);
+
+// "*"
+console.log(num1*num2);
+
+// "/"
+let x = 100;
+let z = 50;
+console.log(x/z);
+console.log((x+z)/2);
+
+// "%" = remaining from the section
+console.log(x%a);
+
+// "++" = +1
+let q = 5;
+// +1
+q++;
+// +1
+q++;
+console.log(q);
+
+// "--" = -1
+q--;
+q--;
+console.log(q);
+
+// "**" = take the exponent
+let no1 = 2;
+let no2 = 4;
+// no1^no2 "2*2*2*2" = 2^4 = 16
+console.log(2**4);
