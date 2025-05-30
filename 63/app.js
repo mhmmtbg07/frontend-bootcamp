f6682be4f29d44354a137064687d9b6135a60049
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-05-30 07:13:19 +0300

    Course 63

---
+++ 63/app.js
@@ -0,0 +1,33 @@
+/* logical Operators
+    !=
+    >
+    <
+    >=
+    <=
+*/
+
+// "!="
+let age = 18;
+console.log(age!=20);
+
+// ">"
+console.log(age>10);
+
+// "<"
+console.log(age<20);
+
+// banner
+console.log("this is a banner");
+
+// ">="
+console.log(age>=18);
+console.log(age>=20);
+console.log(age>=15);
+
+// banner
+console.log("this is a banner");
+
+// "<="
+console.log(age<=18);
+console.log(age<=20);
+console.log(age<=15);
