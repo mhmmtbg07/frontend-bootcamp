8fab3650553095b156bbe328b32995ba62f26e56
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-05-30 12:16:04 +0300

    Course 68

---
+++ 68/app.js
@@ -0,0 +1,18 @@
+/*
+    if/else syntax
+*/
+
+let yol = prompt("Lütfen bir yol seçiniz");
+
+if(yol==1){
+    document.writeln("Seçilen yol " + yol + ".yoldur...");
+}
+else if(yol==2){
+    document.writeln("Seçilen yol " + yol + ".yoldur...");
+}
+else if(yol==3){
+    document.writeln("Seçilen yol " + yol + ".yoldur...");
+}
+else{
+    document.writeln("Lütfen geçerli bir yol seçiniz !!");
+}
