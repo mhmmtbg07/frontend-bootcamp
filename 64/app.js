bd89798cdd8afcf66f2a83681f987aeaa21bc9d1
Muhammet BAĞ <muhammetbag07@gmail.com>
2025-05-30 07:34:35 +0300

    Course 64

---
+++ 64/app.js
@@ -0,0 +1,18 @@
+/*  Dialogue Boxes
+
+    - Alert
+    - Prompt
+    -Confirm
+*/
+
+// "Alert"
+alert("Hello World!"); //string
+
+
+// "Prompt"
+let age = prompt("age : ") // string
+console.log(age);
+
+// "Confirm"
+let access = confirm("Are you sure to do this settings?"); // this is a boolean
+console.log(access);
