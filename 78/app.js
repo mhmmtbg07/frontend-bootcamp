eb8442a8e689fb4ea34c136477844628cffbba05
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-06-05 09:26:54 +0300

    Course 78

---
+++ 78/app.js
@@ -0,0 +1,16 @@
+/* Loop "For" Syntax */
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+}
+
+console.clear();
+
+for(j=0; j <= 100; j = j+2){
+    console.log(j)
+}
+
+console.clear();
+debugger;
+for(a=1000; a >= 5; a=a-5){
+    console.log(a);
+}
