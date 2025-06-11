6c9f83369b66ea9a39768fe747b91fbd9cd74c1a
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-06-11 23:52:22 +0300

    Course 79

---
+++ 79/app.js
@@ -0,0 +1,13 @@
+// While Syntax
+let counter = 1;
+while(counter <= 10){
+    console.log(counter);
+    counter++;
+}
+
+while(true){
+    if(counter == 7){
+        break;
+    }
+    counter++;
+}
