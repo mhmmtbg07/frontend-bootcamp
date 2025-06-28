60da23bfaac598db297f1aea645d4836be62e791
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-06-28 10:08:29 +0300

    Course 88

---
+++ 88/app.js
@@ -0,0 +1,20 @@
+// Methods ( Functions )
+
+/*
+    Method Syntax
+    1* create a method
+    2* write code in this method
+    3* run this method with using method name like "line 19"
+*/
+
+// calculate = method name it can be anything for your process
+function calculate (){
+    document.writeln("Hello World!");
+}
+
+function write (){
+    console.log("Hello World!")
+}
+
+calculate();
+write();
