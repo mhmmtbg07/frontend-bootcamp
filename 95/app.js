29f5f36cc9daf1529003360344f2f3e8f4c212b9
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-07-02 10:32:36 +0300

    Course 95

---
+++ 95/app.js
@@ -0,0 +1,10 @@
+// Arrays
+
+// index starts at 0,1,2,3,4,5,6,
+// Muhammet = 0 / Enes = 1 / Mert = 2 / Özden = 3;
+let names = ["Muhammet" , "Enes" , "Mert" , "Özden"];
+
+// index No
+names [1] = "Mehmet";
+
+console.log (names);
