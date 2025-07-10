291a172003595310ef77aa27ab39c4818369288e
Muhammet Bağ <bagmuhammet07@gmail.com>
2025-07-10 09:49:48 +0300

    Course 97

---
+++ 97/app.js
@@ -0,0 +1,91 @@
+/* array methods */
+
+/*
+    push : appends an erray to the end and returns the data
+    unshift :
+
+    pop :
+    shift :
+
+    splice(index , incdex) :
+
+    toString :
+    join :
+
+    concat :
+    slice :
+    length :
+    reverse :
+    split :
+    indexof :
+    includes :
+*/
+
+// push
+let cars = ["Porche" , "BMW" , "Audi"];
+let cars2 = ["Tofaş" , "Mahawa" , "Crypton"]
+
+// // cars 
+// // 3 item
+// console.log (cars);
+// console.log (cars.length);
+
+// // return the data and value is = "lengths"
+// let lengths = cars.push ("Opel");
+
+// // 4 item
+// console.log (cars);
+// console.log (lengths);
+// console.clear();
+
+// // unshift
+
+// cars.unshift("Hyundai");    // add 1 more array to last
+// console.log(cars);
+// console.log(clear);
+
+// // POP
+
+// cars.pop(); // remove last index
+// console.log(cars);
+// console.clear();
+
+// // Shift
+
+// cars.shift();   // Remove first index
+// console.log(cars);
+
+// splice
+
+// cars.splice(0 , 0 , "Tofas");  //   1: start 2: remove count 3: add to start location 
+// console.log(cars);
+
+// Join
+
+// cars.join("-");
+// console.log(cars);
+
+// Concat
+// let concat = cars.concat(cars2);
+// console.log(concat);
+
+// reverse
+
+// cars.reverse(); // reverse the array
+// console.log(cars);
+
+// split
+
+// let names = "Muhammet,Enes,Mert";
+// let namesArray = names.split(",");
+// console.log(namesArray);
+
+// indexof 
+
+// let index = cars.indexOf("BMW");
+// console.log(index);
+
+// Include
+
+// let value = cars.includes("porche"); // boolean
+// console.log(value);
